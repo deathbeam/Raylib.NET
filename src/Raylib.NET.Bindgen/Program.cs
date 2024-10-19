e78eec0cee8e662e7bc557a84add15ab681df323
Tomas Slusny <slusnucky@gmail.com>
2024-10-19 18:34:44 +0200

    More stuff for bindgen

--- src/Raylib.NET.Bindgen/Program.cs
+++ src/Raylib.NET.Bindgen/Program.cs
@@ -30,10 +30,33 @@ var generatedNamespace = "Raylib.NET";
 var ouputPath = "../../src/Raylib.NET";
 var libPath = "../../lib";
 
-string[] systemIncludeFolders = {
-    "/usr/lib/clang/18/include/",
-    "/usr/include"
-};
+List<string> systemIncludeList = new();
+
+foreach (var include in Directory.GetDirectories("/usr/lib/clang", "*", SearchOption.AllDirectories))
+{
+    string includePath = Path.Combine(include, "include");
+    if (Directory.Exists(includePath))
+    {
+        Console.WriteLine(includePath);
+        systemIncludeList.Add(includePath);
+    }
+}
+
+foreach (var include in Directory.GetDirectories("/usr/lib/", "llvm-*", SearchOption.AllDirectories))
+{
+    string includePath = Path.Combine(include, "include/c++/v1");
+    if (Directory.Exists(includePath))
+    {
+        Console.WriteLine(includePath);
+        systemIncludeList.Add(includePath);
+    }
+}
+
+systemIncludeList.Add("/usr/include");
+string[] systemIncludeFolders = systemIncludeList.ToArray();
+
+// Find "/usr/lib/clang/*/include/",
+// Find "/usr/lib/llvm-*/include/c++/v1/",
 
 string[] includeFolders = { $"{libPath}/raylib/src" };
 
