fe8fe6faa54db077bfdc70341e61c377327260e1
Tomas Slusny <slusnucky@gmail.com>
2024-10-24 22:38:11 +0200

    Add back raygui sample, now it also works with wasm [ci skip]

--- src/Raylib.NET.Example/Program.cs
+++ src/Raylib.NET.Example/Program.cs
@@ -1,8 +1,10 @@
 using RaylibNET;
 using static RaylibNET.Raylib;
+using static RaylibNET.Raygui;
 
+bool showMessageBox = false;
 
-InitWindow(640, 480, "Raylib.NET - Test");
+InitWindow(640, 480, "Raylib.NET - Example");
 
 while (!WindowShouldClose())
 {
@@ -11,6 +13,19 @@ while (!WindowShouldClose())
     ClearBackground(Color.RAYWHITE);
 
     DrawText("Congrats! You created your first window!", 10, 10, 20, Color.LIGHTGRAY);
+
+    if (GuiButton(new(40, 40, 120, 30), "#191#Show Message") == 1)
+    {
+        showMessageBox = true;
+    }
+    if (showMessageBox)
+    {
+        int result = GuiMessageBox(new(85, 70, 250, 100), "#191#Message Box", "Hi! This is a message!", "Nice;Cool");
+        if (result >= 0)
+        {
+            showMessageBox = false;
+        }
+    }
 
     EndDrawing();
 }
