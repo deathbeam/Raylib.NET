202210a38f7ba87b95a4497aad621ae07ad352b6
Tomas Slusny <slusnucky@gmail.com>
2025-10-02 00:20:40 +0200

    use old build again
    Signed-off-by: Tomas Slusny <slusnucky@gmail.com>

--- src/Raylib.NET.Native/build.zig
+++ src/Raylib.NET.Native/build.zig
@@ -1,59 +1,77 @@
 const std = @import("std");
-const rl = @import("raylib");
+const builtin = @import("builtin");
+
+pub fn compileRaylib(b: *std.Build, target: std.Build.ResolvedTarget, optimize: std.builtin.OptimizeMode, shared: bool) !*std.Build.Step.Compile {
+    const raylib = b.dependency("raylib", .{ .target = target, .optimize = optimize, .linkage = if (shared) std.builtin.LinkMode.dynamic else std.builtin.LinkMode.static, .linux_display_backend = .X11 });
+    const raygui = b.dependency("raygui", .{ .target = target, .optimize = optimize });
+    const lib = raylib.artifact("raylib");
+
+    switch (target.result.os.tag) {
+        // Due to *terrible* zig default behaviour for include paths when cross-compiling I have to do this
+        // The includes are resolved properly only when -Dtarget=native (or omitted) is passed
+        .linux => {
+            if (target.result.cpu.arch == .aarch64) {
+                lib.addLibraryPath(.{ .cwd_relative = "/usr/lib/aarch64-linux-gnu/" });
+                lib.addIncludePath(.{ .cwd_relative = "/usr/include/aarch64-linux-gnu/" });
+                lib.addSystemIncludePath(.{ .cwd_relative = "/usr/include" });
+            } else if (target.result.cpu.arch == .x86) {
+                lib.addLibraryPath(.{ .cwd_relative = "/usr/lib/i386-linux-gnu/" });
+                lib.addIncludePath(.{ .cwd_relative = "/usr/include/i386-linux-gnu/" });
+                lib.addSystemIncludePath(.{ .cwd_relative = "/usr/include" });
+                // https://github.com/ziglang/zig/issues/7935
+                lib.link_z_notext = true;
+            } else if (target.result.cpu.arch == .x86_64) {
+                lib.addLibraryPath(.{ .cwd_relative = "/usr/lib/x86_64-linux-gnu/" });
+                lib.addIncludePath(.{ .cwd_relative = "/usr/include/x86_64-linux-gnu/" });
+                lib.addIncludePath(.{ .cwd_relative = "/usr/include" });
+            } else {
+                lib.addSystemIncludePath(.{ .cwd_relative = "/usr/include" });
+            }
+
+            lib.addLibraryPath(.{ .cwd_relative = "/usr/lib" });
+        },
+        else => {}
+    }
+
+    var gen_step = b.addWriteFiles();
+    lib.step.dependOn(&gen_step.step);
+
+    var cflags = std.ArrayList([]const u8){};
+    defer cflags.deinit(b.allocator);
+
+    try cflags.appendSlice(b.allocator, &[_][]const u8{
+        "-std=gnu99",
+        "-D_GNU_SOURCE",
+        "-DGL_SILENCE_DEPRECATION=199309L",
+        "-fno-sanitize=undefined",
+    });
+
+    if (shared) {
+        try cflags.appendSlice(b.allocator, &[_][]const u8{
+            "-fPIC",
+            "-DBUILD_LIBTYPE_SHARED",
+        });
+    }
+
+    lib.addCSourceFile(.{
+        .file = gen_step.add("raygui.c", "#define RAYGUI_IMPLEMENTATION\n#include \"raygui.h\"\n"),
+        .flags = cflags.items,
+    });
+
+    lib.addIncludePath(raylib.path("src"));
+    lib.addIncludePath(raygui.path("src"));
+    lib.installHeader(raygui.path("src/raygui.h"), "raygui.h");
+
+    return lib;
+}
 
 pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const options = rl.Options.getOptions(b);
-
-    const raylib_dep = b.dependency("raylib", .{
-        .target = target,
-        .optimize = optimize,
-        .raudio = options.raudio,
-        .rmodels = options.rmodels,
-        .rshapes = options.rshapes,
-        .rtext = options.rtext,
-        .rtextures = options.rtextures,
-        .platform = options.platform,
-        .linkage = options.linkage,
-        .linux_display_backend = options.linux_display_backend,
-        .opengl_version = options.opengl_version,
-        .android_api_version = options.android_api_version,
-        .android_ndk = options.android_ndk,
-    });
-    const raylib = raylib_dep.artifact("raylib");
-
-    // Add platform-specific include/library paths for cross-compiling
-    switch (target.result.os.tag) {
-        .linux => {
-            if (target.result.cpu.arch == .aarch64) {
-                raylib.addLibraryPath(.{ .cwd_relative = "/usr/lib/aarch64-linux-gnu/" });
-                raylib.addIncludePath(.{ .cwd_relative = "/usr/include/aarch64-linux-gnu/" });
-                raylib.addSystemIncludePath(.{ .cwd_relative = "/usr/include" });
-            } else if (target.result.cpu.arch == .x86) {
-                raylib.addLibraryPath(.{ .cwd_relative = "/usr/lib/i386-linux-gnu/" });
-                raylib.addIncludePath(.{ .cwd_relative = "/usr/include/i386-linux-gnu/" });
-                raylib.addSystemIncludePath(.{ .cwd_relative = "/usr/include" });
-                raylib.link_z_notext = true;
-            } else if (target.result.cpu.arch == .x86_64) {
-                raylib.addLibraryPath(.{ .cwd_relative = "/usr/lib/x86_64-linux-gnu/" });
-                raylib.addIncludePath(.{ .cwd_relative = "/usr/include/x86_64-linux-gnu/" });
-                raylib.addIncludePath(.{ .cwd_relative = "/usr/include" });
-            } else {
-                raylib.addSystemIncludePath(.{ .cwd_relative = "/usr/include" });
-            }
-            raylib.addLibraryPath(.{ .cwd_relative = "/usr/lib" });
-        },
-        else => {},
+    if (target.result.os.tag != .emscripten) {
+        b.installArtifact(try compileRaylib(b, target, optimize, true));
     }
 
-    const raygui_dep = b.dependency("raygui", .{
-        .target = target,
-        .optimize = optimize,
-    });
-
-    rl.addRaygui(b, raylib, raygui_dep, options);
-
-    b.installArtifact(raylib);
+    b.installArtifact(try compileRaylib(b, target, optimize, false));
 }
